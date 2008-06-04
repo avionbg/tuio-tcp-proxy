19619312dd52d7cfbb899cd5270750ac630e64e2
avion.bg@gmail.com <avion.bg@gmail.com@b0bc14e2-e34d-0410-a509-472e5fdc43dc>
2008-06-04 12:33:14 +0000

    added configuration dialog, osc autostarting with toxy (forking app) etc.

--- trunk/make_toxy_win.sh
+++ trunk/make_toxy_win.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#WINDRES =
 i686-mingw32-windres -i toxy_win.rc -o icon.o -O coff
-i686-mingw32-gcc toxy_win.c icon.o -o toxy_win.exe -static -mwindows -llo -lpthread -lws2_32 -I/opt/local/static/include -L/opt/local/static/lib
+#i686-mingw32-windres -i toxy_win.res -o icon.o -O coff
+i686-mingw32-gcc toxy_win.c icon.o -o toxy_win.exe -static -mwindows -lshlwapi -lcomdlg32 -lcomctl32 -llo -lpthread -lws2_32 -I/opt/local/static/include -L/opt/local/static/lib
 i686-mingw32-strip toxy_win.exe
