740e873d21e7972dbcbddea56db6be1ffd9b86cb
xbenlau <xbenlau@b0bc14e2-e34d-0410-a509-472e5fdc43dc>
2009-02-06 07:40:19 +0000

    [Refactor] Moved all of the functions except main() from toxy.c to libtoxy.c . libtoxy.c contains library code that shared with different exec.

--- trunk/Makefile
+++ trunk/Makefile
@@ -6,8 +6,8 @@ all: tuio_tcp_proxy tuio_dump toxy_dump toxy toxy_gtk
 tuio_tcp_proxy: tuio_tcp_proxy.c
 	gcc -llo $< -o $@
 
-toxy: toxy.c
-	gcc -llo $< -o $@
+toxy: toxy.o libtoxy.o 
+	gcc -llo $^ -o $@
 
 toxy_dump: toxy_dump.c
 	gcc -llo $< -o $@
@@ -18,6 +18,9 @@ tuio_dump: tuio_dump.c
 toxy_gtk: toxy_gtk.c
 	${CC} `pkg-config --cflags --libs gtk+-2.0` -o $@ $<
 
+%.o : %.c
+	gcc -c $< 
 
 .PHONY clean:
 	rm -f tuio_dump toxy_dump tuio_tcp_proxy toxy
+	rm -f *.o
---
+++ trunk/libtoxy.c
@@ -0,0 +1,373 @@
+/*
+ *  tuio_socket_proxy.c
+ *
+ *  Tuio protocol proxying (forwarding from udp to server on tcp port)
+ *
+ *  Author : Goran Medakovic
+ *  Email : avion.bg@gmail.com
+ *
+ *  Copyright (c) 2008 Goran Medakovic <avion.bg@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+/* How to compile:
+ * Linux:
+ * gcc tuio_tcp_sproxy.c -o tuio_tcp_sproxy -Wall `pkg-config --cflags --libs liblo`
+ * Windows:
+ * i686-mingw32-gcc tuio_tcp_sproxy.c -o tuio_tcp_sproxy.exe \
+ *  -llo -lws2_32 -lpthread -L/path/to/libs -I/path/to/includes
+ */
+
+#ifdef WIN32
+#include <windows.h>
+#include <winsock.h>
+#define SHUT_RDWR SD_BOTH
+#elif linux
+#define closesocket close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h> //*for TCP_NODELAY
+#include <netdb.h>
+#include <fcntl.h>
+#endif
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+#include "lo/lo.h"
+#include "libtoxy.h"
+
+#define	QLEN 5
+struct protoent *ptrp;
+struct sockaddr_in address;
+int servsock, clientsock, fwdport;
+int done = 0;
+int toggle = 1;
+int debug = 0;
+char *port;
+lo_server st;
+
+typedef struct
+{
+    char path[12];
+    char name[8];
+    int sessionID;
+    float x,y,X,Y,m,w,h;
+} set;
+
+typedef struct
+{
+    char path[12];
+    char name[8];
+    int framenum;
+} fseq;
+
+void error(int num, const char *m, const char *path);
+int quit_handler(const char *path, const char *types, lo_arg **argv, int argc,
+                 void *data, void *user_data);
+int fwd_handler(const char *path, const char *types, lo_arg **argv,
+                    int argc, void *data, void *user_data);
+void initlo();
+void initcp();
+
+#ifdef WIN32
+static void
+set_errno(int winsock_err)
+{
+    switch (winsock_err)
+    {
+    case WSAEWOULDBLOCK:
+        errno = EAGAIN;
+        break;
+    default:
+        errno = winsock_err;
+        break;
+    }
+}
+
+int mingw_setnonblocking (SOCKET fd, int nonblocking)
+{
+    int rc;
+
+    unsigned long mode = 1;
+    rc = ioctlsocket(fd, FIONBIO, &mode);
+    if (rc != 0)
+    {
+        set_errno(WSAGetLastError());
+    }
+    return (rc == 0 ? 0 : -1);
+}
+#endif
+
+void initcp()
+{
+#ifdef WIN32
+    WSADATA wsaData;
+    WSAStartup(0x0101, &wsaData);
+#endif
+    char *opt;
+    memset((char *)&address,0,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons((u_short)fwdport);
+
+    if ( ((int)(ptrp = getprotobyname("tcp"))) == 0)
+    {
+        fprintf(stderr, "cannot map \"tcp\" to protocol number");
+        exit(1);
+    }
+    servsock = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+#ifdef WIN32
+    setsockopt(servsock,SOL_SOCKET,SO_REUSEADDR,(void *)&opt, sizeof(opt));
+    mingw_setnonblocking(servsock, 1);
+#elif linux
+    int size = 1024;
+    if (-1 == setsockopt(servsock, SOL_SOCKET, SO_REUSEADDR | TCP_NODELAY | SO_SNDBUF,
+        &size, sizeof(size)))
+        perror("setsockopt");
+    fcntl(servsock, F_SETFL, O_NONBLOCK);
+    signal(SIGPIPE,SIG_IGN);
+#endif
+    if (servsock < 0)
+    {
+        fprintf(stderr, "socket creation failed\n");
+        exit(1);
+    }
+    if (bind(servsock, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        fprintf(stderr,"bind failed\n");
+        exit(1);
+    }
+    if (listen(servsock, QLEN) < 0)
+    {
+        fprintf(stderr,"listen failed\n");
+        exit(1);
+    }
+
+}
+void stoptcp()
+{
+    int err;//,status;
+    if (servsock)
+    {
+    err = shutdown (servsock, SHUT_RDWR);
+    //printf("Error: %d",err);
+    fflush(stdout);
+    if (err <0) perror("Error shutting down serversock\n");
+    //closesocket(clientsock);
+    closesocket(servsock);
+    }
+/*    if (clientsock)
+    {
+    err = shutdown (clientsock, SHUT_RDWR);
+    if (err <0) perror("Error shutting down clientsock\n");
+    //closesocket(clientsock);
+    closesocket(clientsock);
+    }*/
+    if ( err == 0 )
+    {
+        printf ("restarting tcp\n");
+        fflush(stdout);
+        //lo_server_thread_free(st);
+#ifdef WIN32
+        Sleep(100);
+#elif linux
+        usleep(100000);
+#endif
+        initcp();
+        //initlo();
+    }
+}
+
+void initlo()
+{
+    st = lo_server_new(port, error);
+    lo_server_add_method(st, NULL, NULL, quit_handler, NULL);
+    lo_server_add_method(st, NULL, NULL, fwd_handler, NULL);
+    //lo_server_thread_start(st);
+}
+
+void error(int num, const char *msg, const char *path)
+{
+    printf("liblo server error %d in path %s: %s\n", num, path, msg);
+    fflush(stdout);
+}
+
+int fwd_handler(const char *path, const char *types, lo_arg **argv,
+                    int argc, void *data, void *user)
+{
+    int alen,j;
+    alen = sizeof(address);
+
+    if ( !clientsock || clientsock < 0 )
+        {
+        clientsock=accept(servsock, (struct sockaddr *)&address, &alen);
+        /*if (clientsock <0)
+            if (errno == EAGAIN)
+                printf("EAGAIN\n");*/
+        //perror("error pipe?? \n");//EAGAIN
+        }
+    if ( !strcmp((char *) argv[0],"set") )
+    {
+        set *msgset;
+        int status;
+        msgset = calloc(1, sizeof(set));
+        strcpy(msgset->path, path);
+        strcpy (msgset->name, (char *) argv[0]);
+        msgset->sessionID = argv[1]->i;
+        msgset->x = argv[2]->f;
+        msgset->y = argv[3]->f;
+        msgset->X = argv[4]->f;
+        msgset->Y = argv[5]->f;
+        msgset->m = argv[6]->f;
+        if (argc > 7)
+        {
+            msgset->w = argv[7]->f;
+            msgset->h = argv[8]->f;
+        }
+        if (debug) printf ("path: %s name: %s ID: %d x: %f y: %f X: %f Y: %f m: %f\n",msgset->path, msgset->name, msgset->sessionID, msgset->x, msgset->y, msgset->X, msgset->Y, msgset->m);
+            status = send(clientsock,(void *) msgset,sizeof(*msgset),0);
+            if (status < 0)
+            {
+ /*           perror("set (send) failed");
+            if (errno == EPIPE)
+                printf("EPIPE\n");*/
+                //if (shutdown(clientsock,SHUT_RDWR) <0)
+                //closesocket(servsock);
+                //closesocket(clientsock);
+            closesocket(clientsock);
+            clientsock = 0;
+            }
+        free (msgset);
+    fflush(stdout);
+    }
+    else if ( !strcmp((char *) argv[0],"fseq") && argv[1]->i != -1 )
+    {
+        fseq *msgset;
+        int status;
+        msgset = calloc (1, sizeof(fseq));
+        strcpy(msgset->path, path);
+        strcpy (msgset->name, (char *) argv[0]);
+        msgset->framenum = argv[1]->i;
+        if (debug) printf("path: %s name: %s fremseq: %d\n",msgset->path, msgset->name, msgset->framenum);
+        alen = sizeof(address);
+            status = send(clientsock,(void *) msgset,sizeof(*msgset),0);
+            if (status < 0)
+            {
+            /*if (status == EPIPE)
+                printf("EPIPE\n");
+            perror("fseq (send) failed");*/
+                //if (shutdown(clientsock,SHUT_RDWR) <0)
+                //closesocket(servsock);
+                //closesocket(clientsock);
+            closesocket(clientsock);
+            clientsock = 0;
+            }
+        free(msgset);
+    fflush(stdout);
+    }
+    else if ( !strcmp((char *) argv[0],"alive") )
+    {
+        struct _alive {
+        char path[12];
+        char name[8];
+        int blobs[argc];
+        };
+
+        struct _alive *msgset;
+        int status;
+        msgset = calloc (1, sizeof(struct _alive));
+
+        strcpy(msgset->path, path);
+        strcpy (msgset->name, (char *) argv[0]);
+        msgset->blobs[0] = argc;
+        if (debug) printf("path: %s name: %s ",msgset->path, msgset->name);
+        for (j=1;j<argc;j++)
+        {
+            msgset->blobs[j] = argv[j]->i;
+            if (debug) printf("Id[%d] - %d ",j-1,msgset->blobs[j]);
+        }
+        if (debug) printf("\n");
+            status = send(clientsock,(void *) msgset,sizeof(*msgset),0);
+            if (status < 0)
+            {
+            /*if (errno == EPIPE)
+                printf("EPIPE\n");*/
+            //if (errno == ECONNRESET)
+                //printf("ECONNRESET\n");
+            //printf("errno %d\n",errno);
+                //if (shutdown(clientsock,SHUT_RDWR) <0)
+                //closesocket(servsock);
+            closesocket(clientsock);
+            clientsock = 0;
+            /*perror("alive (send) failed");*/
+                //stoptcp();
+            }
+        free(msgset);
+    fflush(stdout);
+    }
+    return 0;
+}
+
+int qquit_handler(const char *path, const char *types, lo_arg **argv, int argc,
+                 void *data, void *user_data)
+{
+    if ( !strcmp((char *) path ,"/quit"))
+    {
+    done = 1;
+    //if (debug)
+    printf("Quiting\n\n");
+    fflush(stdout);
+    return 0;
+    }
+    else return toggle;
+}
+
+void sigint_handler(int sig)
+{
+    printf("Quiting! %d\n",done);
+    //done = 1;
+}
+
+int quit_handler(const char *path, const char *types, lo_arg **argv, int argc,
+                 void *data, void *user_data)
+{
+    if ( !strcmp((char *) path ,"/quit"))
+    {
+    done = 1;
+    return 0;
+    }
+    //else if ( !strcmp((char *) path ,"/toggle"))
+    //{
+            //toggle = !toggle;
+        //if (connected < 0)
+        //{
+        //if (!toggle) ModifyTrayIcon( hWndtray, ID_TRAYICON, ID_TRAYICON, "Tuio processing stopped.." );
+        //else ModifyTrayIcon( hWndtray, ID_TRAYICON, ID_TRAYICON, "Tuio processing enabled.." );
+        //return 0;
+        //}
+        //else if (connected && toggle)
+            //ModifyTrayIcon( hWndtray, ID_TRAYICON, ID_TRAYICONB, "Tuio processing enabled.." );
+        //else if (!connected && toggle ) ModifyTrayIcon( hWndtray, ID_TRAYICON, ID_TRAYICONR, "Tuio processing stopped.." );
+        //else ModifyTrayIcon( hWndtray, ID_TRAYICON, ID_TRAYICON, "Tuio processing stoped.." );
+    //}
+    else if (!strcmp((char *) path ,"/tuio/2Dobj") || !strcmp((char *) path ,"/tuio/2Dcur"))
+    {
+        return toggle;
+    }
+    else
+    return 0;
+}
